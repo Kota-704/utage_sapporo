b1b6b25765fd8049b4381ef04d1a7cbd9724bb52
Kota-704 <kktt14590358@gmail.com>
2024-11-18 22:45:59 +0900

    ♻️ /キャンペーンセクションの微修正

--- components/campaign/campaign.js
+++ components/campaign/campaign.js
@@ -5,10 +5,19 @@ swiperの設定
 const swiper = new Swiper(".swiper_campaign", {
   slidesPerView: 3,
   loop: true,
-  loopAdditionalSlides: 1,
-  // autoplay: {
-  //   delay: 3000,
-  // },
+  loopAdditionalSlides: 0,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 10,
+    depth: 350,
+    modifier: 1,
+    slideShadows: false,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
