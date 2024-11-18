1803fc49aa4cc795bcfcf51091bf022790e7ae15
Kota-704 <kktt14590358@gmail.com>
2024-11-18 22:45:38 +0900

    ✨ /アクセスセクションの追加

---
+++ components/component_access/component_access.php
@@ -0,0 +1,17 @@
+
+
+<h2 class="section_title oswald_M">ACCESS</h2>
+<div class="access_layout flex">
+  <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/top/access_map.png" alt="access_map">
+  <div class="access_right_contents flex">
+    <p class="hour noto_M">OPENING HOUR <span class="noto_R">22:00- 4:30</span></p>
+    <p class="address noto_M">〒064-0805<br />
+     北海道札幌市中央区南５条西３丁目６−１ 札幌東宝公楽ビル B1F
+    </p>
+      <a href="tel:011-211-6918" class="access_contact_link oswald_R" aria-label="電話番号 011-211-6918に電話をかける">TEL : 011-211-6918</a>
+      <span class="access_contact_link oswald_R">FAX : 011-211-6919</span>
+      <a href="mailto:INFO@UTAGECONTACT.COM" class="access_contact_link oswald_R" aria-label="メールアドレス INFO@UTAGECONTACT.COM にメールを送る">MAIL : INFO@UTAGECONTACT.COM</a>
+  </div>
+
+  </div>
+</div>
