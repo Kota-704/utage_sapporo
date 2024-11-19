4e7cb12d428da9cdfc1818fbe61d8a1882e2eb89
Kota-704 <kktt14590358@gmail.com>
2024-11-19 18:47:57 +0900

    ♻️ /ボタン周り調整

--- components/header/header.js
+++ components/header/header.js
@@ -5,12 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const hamburgerContent = document.querySelector(".hamburger_content");
   const headerNav = document.querySelector(".header_nav");
+  const hamburgerLinks = document.querySelectorAll(".hamburger_link");
 
   if (hamburger && hamburgerContent && headerNav) {
     hamburger.addEventListener("click", () => {
       hamburger.classList.toggle("active");
       hamburgerContent.classList.toggle("active");
       headerNav.classList.toggle("active");
+    });
+
+    hamburgerLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        hamburgerContent.classList.remove("active");
+        headerNav.classList.remove("active");
+      });
     });
   }
 });
