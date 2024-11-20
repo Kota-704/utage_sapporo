2280cc4d801f462ccdb000dbdaa57abe8dc52f92
Kota-704 <kktt14590358@gmail.com>
2024-11-20 22:02:18 +0900

    ♻️ /リンクに飛べないバグの修正

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,21 +5,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".header_link, .hamburger_link").forEach((link) => {
     link.addEventListener("click", function (e) {
-      e.preventDefault();
-
       const href = this.getAttribute("href");
-      const targetId = href.includes("#") ? href.split("#")[1] : null;
-      const targetElement = targetId ? document.getElementById(targetId) : null;
 
-      if (targetElement) {
-        const headerHeight = document.querySelector(".header_nav").offsetHeight;
-        const targetPosition = targetElement.offsetTop - headerHeight;
+      if (href && href.includes("#")) {
+        e.preventDefault();
 
-        window.scrollTo({
-          top: targetPosition,
-          behavior: "smooth",
-        });
+        const targetId = href.split("#")[1];
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          const headerHeight =
+            document.querySelector(".header_nav").offsetHeight;
+          const targetPosition = targetElement.offsetTop - headerHeight;
+
+          window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth",
+          });
+        }
       }
+      // ハッシュリンク以外の場合は通常のページ遷移を許可
     });
   });
 });
