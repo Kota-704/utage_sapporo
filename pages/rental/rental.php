528a4e0812cd4e63cab7a5898aefa4895e3eb08f
Kota-704 <kktt14590358@gmail.com>
2024-11-21 22:23:27 +0900

    ♻️ /フェードイン機能の改修

--- pages/rental/rental.php
+++ pages/rental/rental.php
@@ -2,15 +2,15 @@
   <section class="rental">
     <div class="w_1280 wrapper">
         <h2 class="rental_title oswald_M">RENTAL</h2>
-        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/rental/rental_image.png" alt="rental_image" class="rental_image">
+        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/rental/rental_image.png" alt="rental_image" class="rental_image fade-in">
     </div>
   </section>
   <section class="reservation">
     <h2 class="section_title oswald_M">RESERVATION</h2>
-    <p class="reservation_text noto_M txt_center">
+    <p class="reservation_text noto_M txt_center fade-in">
         下記フォームよりご予約が可能です。<br />
         不明点等お気軽にお問い合わせください。
     </p>
 
-    <a href="" class="reservation_link oswald_R">RESERVATION</a>
+    <a href="" class="reservation_link oswald_R fade-in">RESERVATION</a>
   </section>
