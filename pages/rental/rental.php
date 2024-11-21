ac06d286b7abdee6aa5589bd1869cbbdb2dc80b2
Kota-704 <kktt14590358@gmail.com>
2024-11-21 22:14:00 +0900

    ✨ /レンタルページ追加

--- pages/rental/rental.php
+++ pages/rental/rental.php
@@ -1,2 +1,16 @@
 
-    <h2 class="rental_title oswald_M">RENTAL</h2>
+  <section class="rental">
+    <div class="w_1280 wrapper">
+        <h2 class="rental_title oswald_M">RENTAL</h2>
+        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/rental/rental_image.png" alt="rental_image" class="rental_image">
+    </div>
+  </section>
+  <section class="reservation">
+    <h2 class="section_title oswald_M">RESERVATION</h2>
+    <p class="reservation_text noto_M txt_center">
+        下記フォームよりご予約が可能です。<br />
+        不明点等お気軽にお問い合わせください。
+    </p>
+
+    <a href="" class="reservation_link oswald_R">RESERVATION</a>
+  </section>
