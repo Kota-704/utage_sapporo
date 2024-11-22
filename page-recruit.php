a7bb61cf4a3c10b7537a59ee21d08d68a0d78d79
Kota-704 <kktt14590358@gmail.com>
2024-11-22 22:42:06 +0900

    ✨ /リクルートページ作成

---
+++ page-recruit.php
@@ -0,0 +1,4 @@
+<?php
+  get_template_part('components/header/header');
+  require_once get_template_directory() . '/pages/recruit/recruit.php';
+  get_template_part('components/footer/footer');
---
+++ pages/recruit/recruit.php
@@ -0,0 +1,55 @@
+  <section class="recruit">
+    <div class="w_1280 wrapper">
+        <h2 class="recruit_title oswald_M">RECRUIT</h2>
+        <p class="top_text txt_center noto_M">「UTAGE SAPPORO」では新たにスタッフ・ダンサー・DJを募集しております！</p>
+        <div class="recruit_top_layout flex">
+          <div class="recruit_top_container">
+            <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/DJ/recruit_dj.jpg" alt="DJ" class="recruit_image">
+            <h3 class="job_heading noto_B">UTAGE STAFF</h3>
+            <span class="horizon"></span>
+            <p class="job_text noto_M">
+              完全未経験でも大募集‼️<br />
+              日払い制度！<br />
+              深夜時給1438円〜 <br />
+              高収入可能！臨時ボーナスもあり！<br />
+              髪、ネイル自由！
+            </p>
+          </div>
+          <div class="recruit_top_container">
+            <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/DANCER/LINE_ALBUM_20231124_240104.jpg" alt="" class="recruit_image">
+            <h3 class="job_heading dancer_heading noto_B">
+              UTAGE DANCER<br />
+              <span class="supplement noto_R">（シャンパンガール可）</span>
+            </h3>
+            <span class="horizon"></span>
+            <p class="job_text noto_M">
+              有名になりたい！全国で活躍したい！<br />
+              業界高時給！<br />
+              完全未経験でも安心！<br />
+              個別レッスンあり<br />
+              チップバック制度あり
+            </p>
+          </div>
+          <div class="recruit_top_container">
+            <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/DJ/recruit_dj.jpg" alt="DJ" class="recruit_image">
+            <h3 class="job_heading noto_B">UTAGE DJ</h3>
+            <span class="horizon"></span>
+            <p class="job_text noto_M">
+              有名になりたい！全国で活躍したい！<br />
+              完全未経験でも安心！<br />
+              個別レッスンあり<br />
+              スキルに応じて昇給制度
+            </p>
+          </div>
+        </div>
+    </div>
+  </section>
+  <section class="reservation">
+    <h2 class="section_title oswald_M">RESERVATION</h2>
+    <p class="reservation_text noto_M txt_center fade-in">
+        下記フォームよりご予約が可能です。<br />
+        不明点等お気軽にお問い合わせください。
+    </p>
+
+    <a href="" class="reservation_link oswald_R fade-in">RESERVATION</a>
+  </section>
