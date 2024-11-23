fa24dae556eb9817f3034f15202d0b675acb5c99
Kota-704 <kktt14590358@gmail.com>
2024-11-23 14:56:20 +0900

    ✨ /コード追加

--- page-recruit.php
+++ page-recruit.php
@@ -1,4 +1,5 @@
 <?php
   get_template_part('components/header/header');
   require_once get_template_directory() . '/pages/recruit/recruit.php';
+  get_template_part('components/aside/aside');
   get_template_part('components/footer/footer');
--- pages/recruit/recruit.php
+++ pages/recruit/recruit.php
@@ -45,16 +45,60 @@
         <div class="job_description_container">
           <div class="job_description_inner">
             <h3 class="job_description_heading noto_B txt_center">UTAGE STAFF</h3>
+            <span class="horizon"></span>
+            <dl class="job_detail flex noto_R">
+              <dt class="detail_heading">勤務時間</dt>
+              <dd class="detail_text">
+                ・週2日～、1日6H～OK <br />
+                →24:00～翌6:00も可◎ <br />
+                →深夜時間帯/22:00～翌5:00 <br />
+                →勤務時間帯はほぼ深夜時給です♪ <br />
+                金・土出勤できる方歓迎 <br />
+                ※イベント時は上記時間帯以外の早出有
+              </dd>
+              <span class="horizon"></span>
+              <dt class="detail_heading">給与</dt>
+              <dd class="detail_text">
+                ・ [ア・パ]時給1,438円～ <br />
+                 交通費：一部支給（規定あり）<br /><br />
+                 【ゆくゆくは正社員へステップアップしたい方】<br />
+                 * [正]月給30万円～ <br />
+                 ※始めは、アルバイトからの雇用になります。<br />
+                 ※賞与年2回（業績により） <br />
+                 ※昇給制度（年2回）査定により判断 <br />
+                 ※交通費全額支給（1か月分の定期代毎月） <br />
+                 ※公共交通機関の利用限定（タクシーを除く）
+              </dd>
+              <span class="horizon"></span>
+              <dt class="detail_heading">待遇・福利厚生</dt>
+              <dd class="detail_text detail_text_layout grid">
+                <span class="wlfare_text">・日払い制度 </span>
+                <span class="wlfare_text">・制服貸与</span>
+                <span class="wlfare_text">・交通費当社規定支給</span>
+                <span class="wlfare_text">・昇給制度あり</span>
+                <span class="wlfare_text">・深夜手当有</span>
+                <span class="wlfare_text">・駅チカ</span>
+                <span class="wlfare_text">・社員登用制度有</span>
+                <span class="wlfare_text">・髪型自由</span>
+                <span class="wlfare_text">・ネイルOK</span>
+                <span class="wlfare_text">・ピアスOK</span>
+                <span class="wlfare_text">・髭OK</span>
+                <span class="wlfare_text">・WワークOK</span>
+                <span class="wlfare_text">・臨時ボーナス（規定あり）</span>
+                <span class="wlfare_text">・社会保険完備（正社員）</span>
+              </dd>
+            </dl>
           </div>
         </div>
     </div>
   </section>
-  <section class="reservation">
-    <h2 class="section_title oswald_M">RESERVATION</h2>
-    <p class="reservation_text noto_M txt_center fade-in">
-        下記フォームよりご予約が可能です。<br />
+  <section class="entry">
+    <h2 class="section_title oswald_M">ENTRY</h2>
+    <p class="entry_text noto_M txt_center fade-in">
+        下記ボタンよりご応募が可能です。 <br />
         不明点等お気軽にお問い合わせください。
     </p>
 
-    <a href="" class="reservation_link oswald_R fade-in">RESERVATION</a>
+    <a href="" class="entry_link oswald_R fade-in">ENTRY</a>
+
   </section>
