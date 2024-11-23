d43950119de06c40d07e2702253e032ea88846a8
Kota-704 <kktt14590358@gmail.com>
2024-11-23 11:55:43 +0900

    ✨ /aside追加

--- page-rental.php
+++ page-rental.php
@@ -1,4 +1,5 @@
 <?php
   get_template_part('components/header/header');
   require_once get_template_directory() . '/pages/rental/rental.php';
+  get_template_part('components/aside/aside');
   get_template_part('components/footer/footer');
