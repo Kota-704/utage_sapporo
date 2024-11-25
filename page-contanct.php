75ccf8311d8b2b0a49dd23d448341246f94d4272
Kota-704 <kktt14590358@gmail.com>
2024-11-25 21:33:19 +0900

    ✨ /新規作成

---
+++ page-contanct.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Contact Page
+*/
+  get_template_part('components/header/header');
+  require_once get_template_directory() . '/pages/contact/contact.php';
+  get_template_part('components/aside/aside');
+  get_template_part('components/footer/footer');
---
+++ pages/contact/contact.php
@@ -0,0 +1,19 @@
+<section class="recruit">
+  <div class="w_1280 wrapper">
+
+    <?php
+    // ページを取得
+    $page = get_page_by_path('contact');
+    ?>
+
+    <?php if ($page && is_object($page)) : ?>
+
+      <h2 class="contact_title oswald_M">CONTACT</h2>
+      <div class="page_content">
+          <?php echo apply_filters('the_content', $page->post_content); ?>
+      </div>
+    <?php else : ?>
+      <p>固定ページが見つかりません。</p>
+    <?php endif; ?>
+  </div>
+</section>
