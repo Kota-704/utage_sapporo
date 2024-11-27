1b66e5f2f2974bcbe6910b3e80d08f68d122732b
Kota-704 <kktt14590358@gmail.com>
2024-11-27 20:46:57 +0900

    ✨ /新規作成

---
+++ pages/floor/floor.php
@@ -0,0 +1,13 @@
+<section class="floor">
+    <h2 class="floor_title oswald_M">FLOOR</h2>
+    <div class="w_1280 wrapper">
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/floor/IMG_5411.JPG" alt="店内マップ" class="floor_map_image">
+
+      <div class="floor_image_container grid">
+        <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/floor/640x640_rect_149879745.jpeg" alt="店内画像" class="floor_image">
+        <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/floor/SILVER_VIP.jpg" alt="店内画像" class="floor_image">
+        <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/floor/PLATINUM_VIP.jpg" alt="店内画像" class="floor_image">
+        <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/floor/GOLD_VIP.jpg" alt="店内画像" class="floor_image">
+      </div>
+    </div>
+</section>
