2764936061d7e313c38cbee527983ad848a24c20
Kota-704 <kktt14590358@gmail.com>
2024-11-27 20:58:26 +0900

    ✨ /新規作成

---
+++ pages/system/system.php
@@ -0,0 +1,7 @@
+<section class="system">
+  <h2 class="system_title oswald_M">SYSTEM</h2>
+  <div class="w_1280 wrapper">
+    <h3 class="coming_soon oswald_B txt_center">Coming Soon</h3>
+    <p class="under_maintenance noto_M txt_center">システムページは準備中です。</p>
+  </div>
+</section>
