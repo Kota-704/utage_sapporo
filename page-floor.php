2a90d8b4bd6bc3e2a63d4e0b9c9d7d342a6672b1
Kota-704 <kktt14590358@gmail.com>
2024-11-27 20:46:44 +0900

    ♻️ /パスの変更

---
+++ page-floor.php
@@ -0,0 +1,5 @@
+<?php
+  get_template_part('components/header/header');
+  require_once get_template_directory() . '/pages/floor/floor.php';
+  get_template_part('components/aside/aside');
+  get_template_part('components/footer/footer');
