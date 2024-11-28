9c8ac81b559c9990bff18b3bc1ab469dd8b903e5
Kota-704 <kktt14590358@gmail.com>
2024-11-28 21:07:43 +0900

    ✨ /asideのレスポンシブ追加

--- components/aside/aside.php
+++ components/aside/aside.php
@@ -1,7 +1,8 @@
 <div class="aside">
   <div class="link_container flex">
     <a href="tel:011-211-6918" class="contact_link">
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservations.png" alt="vip_reservations" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservations.png" alt="vip_reservations" class="pc" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservvation_sp.png" alt="vip_reservations" class="tablet" />
     </a>
     <a href="" class="contact_link">
       <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/Instagram_Glyph_White.png" alt="Instagram" />
