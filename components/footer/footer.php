700b400bafe672618084ee566e57390f99a79994
Kota-704 <kktt14590358@gmail.com>
2024-12-01 10:31:14 +0900

    ♻️ /フッターレイアウト修正

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -1,26 +1,28 @@
     <div class="footer">
       <div class="w_1280 wrapper">
-        <p class="address noto_B txt_center">
-          〒064-0805 北海道札幌市中央区南5条西3丁目6-1 札幌東宝公楽ビル B1F
-        </p>
-        <ul class="link_container flex">
-          <li class="link_list">
-            <a href="https://www.instagram.com/utage_sapporo/" class="sns_link"  target="_blank">
-              <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/Instagram_Glyph_White.png" alt="Instagram" />
-            </a>
-          </li>
-          <li class="link_list">
-            <a href="https://x.com/UTAGE_SAPPORO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="sns_link"  target="_blank">
-              <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/logo-white.png" alt="X" />
-            </a>
-          </li>
-          <li class="link_list">
-            <a href="https://www.tiktok.com/@utage_sapporo" class="sns_link"  target="_blank">
-              <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/TikTok-logo-RGB-Stacked-white-simplified.png" alt="TikTok" />
-            </a>
-          </li>
-        </ul>
-        <small class="copyright txt_center m1c">&copy;2024 UTAGE SAPPORO</small>
+        <div class="footer_layout flex">
+          <p class="address noto_B txt_center">
+            〒064-0805 北海道札幌市中央区南5条西3丁目6-1 札幌東宝公楽ビル B1F
+          </p>
+          <ul class="link_container flex">
+            <li class="link_list">
+              <a href="https://www.instagram.com/utage_sapporo/" class="sns_link"  target="_blank">
+                <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/Instagram_Glyph_White.png" alt="Instagram" />
+              </a>
+            </li>
+            <li class="link_list">
+              <a href="https://x.com/UTAGE_SAPPORO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="sns_link"  target="_blank">
+                <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/logo-white.png" alt="X" />
+              </a>
+            </li>
+            <li class="link_list">
+              <a href="https://www.tiktok.com/@utage_sapporo" class="sns_link"  target="_blank">
+                <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/TikTok-logo-RGB-Stacked-white-simplified.png" alt="TikTok" />
+              </a>
+            </li>
+          </ul>
+          <small class="copyright txt_center m1c">&copy;2024 UTAGE SAPPORO</small>
+        </div>
       </div>
     </div>
 
