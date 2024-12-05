251d8d7fa8b6f409bbe2528ca081339ba0bbfd2c
Kota-704 <kktt14590358@gmail.com>
2024-12-05 20:53:10 +0900

    ♻️ /タイトルの修正

--- components/component_news/component_news.php
+++ components/component_news/component_news.php
@@ -22,7 +22,7 @@ $query = new WP_Query($args);
                         </div>
                     <?php endif; ?>
                     <div class="news_content">
-                        <h3 class="news_title noto_B txt_center"><?php the_title(); ?></h3>
+                        <h3 class="news_title noto_B txt_center"><?php echo get_the_date(); ?></h3>
                         <p class="news_description noto_M txt_center">
                             <?php the_excerpt(); ?>
                         </p>
