3c5bcd356d1c6f79ea2e0123b9047173c77c8686
Kota-704 <kktt14590358@gmail.com>
2024-12-08 17:58:26 +0900

    ♻️ /レイアウト調整

--- pages/rental/rental.php
+++ pages/rental/rental.php
@@ -6,11 +6,13 @@
     </div>
   </section>
   <section class="reservation">
-    <h2 class="section_title oswald_M">RESERVATION</h2>
-    <p class="reservation_text noto_M txt_center fade-in">
-        下記フォームよりご予約が可能です。<br />
-        不明点等お気軽にお問い合わせください。
-    </p>
+    <div class="w_1280 wrapper">
+      <h2 class="section_title oswald_M">RESERVATION</h2>
+      <p class="reservation_text noto_M txt_center fade-in">
+          下記フォームよりご予約が可能です。<br />
+          不明点等お気軽にお問い合わせください。
+      </p>
 
-    <a href="<?php echo esc_url(home_url('/contact')); ?>" class="reservation_link oswald_R fade-in">RESERVATION</a>
+      <a href="<?php echo esc_url(home_url('/contact')); ?>" class="reservation_link oswald_R fade-in">RESERVATION</a>
+    <div class="w_1280 wrapper">
   </section>
