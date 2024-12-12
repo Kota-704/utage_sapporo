0df19c5cfada1fc43ab2b8a9cb8fd9b180267e06
Kota-704 <kktt14590358@gmail.com>
2024-12-12 21:43:19 +0900

    ♻️ /画像差し替えによる修正

--- components/aside/aside.php
+++ components/aside/aside.php
@@ -1,17 +1,17 @@
 <div class="aside">
   <div class="link_container flex">
     <a href="tel:011-211-6918" class="contact_link">
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservations.png" alt="vip_reservations" class="pc" />
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservvation_sp.png" alt="vip_reservations" class="tablet_sp" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservations.png" alt="vip_reservations" class="vip_reservations_image pc" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/vip_reservvation_sp.png" alt="vip_reservations" class="vip_reservations_image tablet_sp" />
     </a>
     <a href="https://www.instagram.com/utage_sapporo/" class="contact_link" target="_blank">
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/Instagram_Glyph_White.png" alt="Instagram" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/Instagram.svg" alt="Instagram" />
     </a>
     <a href="https://x.com/UTAGE_SAPPORO?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="contact_link" target="_blank">
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/logo-white.png" alt="X" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/X.svg" alt="X" />
     </a>
     <a href="https://www.tiktok.com/@utage_sapporo" class="contact_link" target="_blank">
-      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/TikTok-logo-RGB-Stacked-white-simplified.png" alt="TikTok" />
+      <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/icons/TikTok.svg" alt="TikTok" />
     </a>
   </div>
 </div>
