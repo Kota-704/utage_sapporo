6c88c2ff832c8e5fcd820aaf209d7b2e7adf6363
Kota-704 <kktt14590358@gmail.com>
2024-12-29 00:17:37 +0900

    ✨ /新規作成

---
+++ pages/attention/attention.php
@@ -0,0 +1,18 @@
+
+
+  <section class="attention">
+    <div class="w_1280 wrapper">
+        <h2 class="attention_title oswald_M">ATTENTION</h2>
+
+        <div class="section_title_layout flex">
+          <h3 class="section_title noto_B">AGE LIMIT</h3>
+          <p class="section_subtitle noto_R">年齢制限</p>
+        </div>
+        <span class="horizon"></span>
+        <p class="description noto_M">
+          原則として18歳未満の入場をお断りしております。日中開催のイベントは、一部ご入場頂けるものもございます。イベントの詳細をご確認下さい。いかなる場合もIDチェックが必須となりますので、必ず写真付きの身分証明書をご持参下さい。
+        </p>
+      </div>
+
+
+  </section>
