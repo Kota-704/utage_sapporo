146fadaadfcf2b4ff8622cd4786f26b9cf070c4e
Kota-704 <kktt14590358@gmail.com>
2024-12-30 21:57:04 +0900

    ✨ /新規ページ追加

--- pages/attention/attention.php
+++ pages/attention/attention.php
@@ -12,6 +12,29 @@
         <p class="description noto_M">
           原則として18歳未満の入場をお断りしております。日中開催のイベントは、一部ご入場頂けるものもございます。イベントの詳細をご確認下さい。いかなる場合もIDチェックが必須となりますので、必ず写真付きの身分証明書をご持参下さい。
         </p>
+        <div class="section_title_layout flex">
+          <h3 class="section_title noto_B">DRESS CODE</h3>
+          <p class="section_subtitle noto_R">ドレスコード</p>
+        </div>
+        <span class="horizon"></span>
+        <p class="description noto_M">
+          特別なドレスコードはございませんが、以下の場合入場をお断りさせていただく場合がございます。<br />
+        ・公の場にそぐわないと服装とスタッフが判断した場合<br />
+        ・サンダルなど過度な軽装で事故の原因になりえると判断した場合<br />
+        ・刺青・タトゥー（シールやペイントを含む）を露出されているお客様
+        </p>
+        <div class="section_title_layout flex">
+          <h3 class="section_title noto_B">CLOAK</h3>
+          <p class="section_subtitle noto_R">ロッカークローク</p>
+        </div>
+        <span class="horizon"></span>
+        <p class="description noto_M">
+          入店後のお荷物についてはロッカーをご用意しております。また、大きなお荷物の場合、クロークもご用意しておりますので、エントランスのスタッフにお申し付けくださいませ。 また万が一、ロッカー等を使用しない荷物について盗難や窃盗などの被害に遭われた場合、当店では一切の責任を負いかねます。ご了承ください。
+        </p>
+
+        <div class="attention_image_wrapper">
+          <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/attention/attention.png)" alt="attention" class="attention_image" />
+        </div>
       </div>
 
 
