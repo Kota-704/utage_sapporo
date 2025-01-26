ca3980daef859fd14fcb8f034193af13ffc97cbb
Kota-704 <kktt14590358@gmail.com>
2025-01-26 23:46:48 +0900

    ♻️ /プラチナ項目追加

--- pages/vip/vip.php
+++ pages/vip/vip.php
@@ -1,26 +1,27 @@
 <section class="vip">
     <h2 class="vip_title oswald_M">VIP</h2>
     <div class="w_1280 wrapper">
-        <div class="vip_container grid">
-            <?php
-            $page_id = get_the_ID();
-            $content = get_post_field('post_content', $page_id);
+      <div class="tab_layout flex oswald_R">
+        <button class="tab-button platinum active" data-tab="platinum">PLATINUM VIP</button>
+        <button class="tab-button gold" data-tab="gold">GOLD VIP</button>
+        <button class="tab-button silver" data-tab="silver">SILVER VIP</button>
+        <button class="tab-button royal" data-tab="royal">ROYAL VIP</button>
+      </div>
 
-            preg_match_all('/<img[^>]+src="([^">]+)"[^>]*alt="([^">]*)"/', $content, $matches);
-
-            if (!empty($matches[1])) :
-                foreach ($matches[1] as $index => $image_url) :
-                    $alt_text = $matches[2][$index];
-            ?>
-                    <div class="vip_item">
-                        <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($alt_text); ?>" class="vip_image">
-                    </div>
-            <?php
-                endforeach;
-            else :
-                echo '<p class="no-images">画像はまだアップロードされていません。</p>';
-            endif;
-            ?>
+      <div class="tab-contents">
+        <div class="tab-content active" id="platinum">
+          <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/VIP/pratinam_0.jpg" alt="pratinum_floor_map" class="vip_img">
+          <img src="<?php echo esc_url(get_template_directory_uri() ); ?>/assets/img/VIP/PLATINUM VIP.jpg" alt="pratinum_floor_image" class="vip_img">
         </div>
+        <div class="tab-content" id="gold">
+          <p>GOLD VIP content here...</p>
+        </div>
+        <div class="tab-content" id="silver">
+          <p>SILVER VIP content here...</p>
+        </div>
+        <div class="tab-content" id="royal">
+          <p>ROYAL VIP content here...</p>
+        </div>
+      </div>
     </div>
 </section>
