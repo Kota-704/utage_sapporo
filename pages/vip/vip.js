2b6b8d4c3d0674e925a8beb22486164f713b9863
Kota-704 <kktt14590358@gmail.com>
2025-01-27 22:00:27 +0900

    ♻️ /コメントアウト修正

--- pages/vip/vip.js
+++ pages/vip/vip.js
@@ -4,14 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   tabButtons.forEach((button) => {
     button.addEventListener("click", () => {
-      // Remove 'active' class from all buttons
       tabButtons.forEach((btn) => btn.classList.remove("active"));
-      // Add 'active' class to the clicked button
       button.classList.add("active");
 
-      // Hide all tab contents
       tabContents.forEach((content) => content.classList.remove("active"));
-      // Show the selected tab content
       const tabId = button.getAttribute("data-tab");
       document.getElementById(tabId).classList.add("active");
     });
